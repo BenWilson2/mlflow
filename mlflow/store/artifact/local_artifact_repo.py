13f5bdd434fb12c2fe852e103e0385a6563bdc78
Ben Wilson <benjamin.wilson@databricks.com>
2024-03-22 22:29:21 -0400

    test fix
    Signed-off-by: Ben Wilson <benjamin.wilson@databricks.com>

--- mlflow/store/artifact/local_artifact_repo.py
+++ mlflow/store/artifact/local_artifact_repo.py
@@ -83,12 +83,10 @@ class LocalArtifactRepository(ArtifactRepository):
         artifact_path = validate_path_is_safe(artifact_path)
         local_artifact_path = os.path.join(self.artifact_dir, os.path.normpath(artifact_path))
 
-        if local_artifact_path.endswith("."):
-            local_artifact_path.rstrip(".")
-
-        if not os.path.exists(local_artifact_path):
-            raise OSError(f"No such file or directory: '{local_artifact_path}'")
-        return os.path.abspath(local_artifact_path)
+        model_abs_path = os.path.abspath(local_artifact_path)
+        if not os.path.exists(model_abs_path):
+            raise OSError(f"No such file or directory: '{model_abs_path}'")
+        return model_abs_path
 
     def list_artifacts(self, path=None):
         # NOTE: The path is expected to be in posix format.
